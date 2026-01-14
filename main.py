027e870ae15c514be5d9870e2b38eebe55a28d3b
Elide <elidepaixao@hotmail.com>
2026-01-14 16:12:32 -0300

    iniciando navegador e endereco

--- main.py
+++ main.py
@@ -5,11 +5,28 @@
 # Enviar as informações para o sistema
 # Repetir o cadastro até acabar o cadastro de todos os produtos
 
-#Passo 1: Entrar no sistema da empresa
+#Passo 1: Entrar no sistema da empresa:  https://dlp.hashtagtreinamentos.com/python/intensivao/login
+
 
 import pandas as pd
 import pyautogui
 import time
+
+pyautogui.PAUSE = 0.5
+
+pyautogui.press("win")
+time.sleep(1)
+pyautogui.write("chrome")
+time.sleep(1)
+pyautogui.press("enter")
+time.sleep(3)
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press("enter")
+time.sleep(5)
+
+#fazer login
+# selecionar o campo de email
+
 
 
 
