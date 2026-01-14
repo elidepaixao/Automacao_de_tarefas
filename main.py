3bfac9a733299343adc593fd1f0b4a91124b8bb9
Elide <elidepaixao@hotmail.com>
2026-01-14 12:34:01 -0300

    Iniciando prpjeto com venv e requirements

---
+++ main.py
@@ -0,0 +1,17 @@
+# O que a autoimação deve fazer?
+# Abrir o navegador
+# Acessar o site do sistema com login e senha
+# Inserir todas as informações do produto
+# Enviar as informações para o sistema
+# Repetir o cadastro até acabar o cadastro de todos os produtos
+
+#Passo 1: Entrar no sistema da empresa
+
+import pandas as pd
+import pyautogui
+import time
+
+
+
+
+
